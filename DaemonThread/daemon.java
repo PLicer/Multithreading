3ca88d7c14127d1711c177754dab56dcfc15b9a1
PLicer <127109652+PLicer@users.noreply.github.com>
2024-03-12 17:37:02 +0530

    Daemon thread creation  using .setDaemon(true)

---
+++ DaemonThread/daemon.java
@@ -0,0 +1,28 @@
+package DaemonThread;
+
+import javax.swing.plaf.TableHeaderUI;
+
+public class daemon extends Thread{
+
+    public void run()
+    {
+        if(Thread.currentThread().isDaemon())
+        {
+            System.out.println("It is a daemon thread");
+            System.out.println(Thread.currentThread().getName());
+        }
+        else {
+            System.out.println("No it is not a daemon thread");
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello this is main thread");
+        System.out.println();
+        daemon d = new daemon();
+        d.setDaemon(true);//It is use to convert thread into daemon thread
+        d.start();
+
+    }
+
+}
